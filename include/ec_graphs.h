1cd7bdbfc236b60911fa4deef1fd01ff5fbb08ad
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2019-08-27 14:02:00 +0200

    Define function for more x points for y = 0

--- include/ec_graphs.h
+++ include/ec_graphs.h
@@ -6,14 +6,17 @@ struct ec_parameters {
 	int b;
 	char sign1;
 	char sign2;
+	double *xs_4_y0;// [3];
 	double *points;// [n];
 	unsigned n;
 };
 
-void ec_init(struct ec_parameters *ec);
+void ec_init(struct ec_parameters *ec, const int memSize);
 //void ec_y_square(const double x, const double a, const double b);
 double x_4_y0(const double a, const double b);
+int all_xs_4_y0(double *xs/*[3]*/, const double a, const double b);
 double critical_proportion(const double a, const double b);
+void ec_free(struct ec_parameters ec);
 
 #endif
 
